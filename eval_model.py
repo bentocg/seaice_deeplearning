3531cfd8d1a6d37f7ed67a9bd40824191672a3cf
bentocg <bentocollares@gmail.com>
2021-06-19 05:35:06 -0400

    final changes to eval model

--- eval_model.py
+++ eval_model.py
@@ -23,11 +23,12 @@ def parse_args():
     parser.add_argument('--num_workers', '-w', type=int, default=4, help='number of workers for dataloader')
     parser.add_argument('--input_folder', '-i', type=str, help='path to input test set')
     parser.add_argument('--masks_folder', '-m', type=str, help='path to folder with groundtruth masks')
+    parser.add_argument('--raw_image_folder', '-f', type=str, help='path to raw images')
     parser.add_argument('--stride', '-s', type=float, default=1, help='stride for prediction')
     parser.add_argument('--tta', '-t', type=bool, default=0, help='toggle for test-time augmentation')
     parser.add_argument('--output_folder','-o', type=str,  default='test_output', help='output folder for test predictions')
     parser.add_argument('--threshold', '-x', type=float, default=0.5, help='threshold for output binarization')
-    parser.add_argument('--save_output', '-u', type=bool, default=1, help='whether to save the output')
+    parser.add_argument('--save_output', '-u', type=bool, default=0, help='whether to save the output')
     
     return parser.parse_args()
 
@@ -71,25 +72,30 @@ def main():
             
 
     # scan input and mask folders
-    os.makedirs(args.output_folder, exist_ok=True)
+    os.makedirs(f'{args.output_folder}/scene_masks/', exist_ok=True)
     masks = []
     for path, _, filenames in os.walk(args.masks_folder):
         for file in filenames:
             if file.endswith('.tif'):
                 masks.append(f'{path}/{file}')
     scenes = [f'{args.input_folder}/{patch_size}/{os.path.basename(ele)}' for ele in masks]
+    raw_images = [f'{args.raw_image_folder}/{os.path.basename(ele)}' for ele in masks]
 
     # store model performance
-    if os.path.exists(f'{args.output_folder}/model_stats.csv'):
-        model_stats = pd.read_csv(f'{args.output_folder}/model_stats.csv')
+    if os.path.exists(f'{args.output_folder}/global_stats.csv'):
+        global_stats = pd.read_csv(f'{args.output_folder}/global_stats.csv')
     else:
-        model_stats = pd.DataFrame()
+        global_stats = pd.DataFrame()
+
+    model_stats = pd.DataFrame()
 
     # loop through image-mask pairs
     for idx, fname in enumerate(scenes):
         scene = os.path.basename(fname)
         mask = cv2.imread(masks[idx], cv2.IMREAD_GRAYSCALE)
+        raw = cv2.imread(raw_images[idx]).astype(np.uint8)
         heigth, width = mask.shape
+        
         # instantiate dataset and dataloder
         dataset = TestDataset(fname)
         dataloader = DataLoader(
@@ -115,7 +121,12 @@ def main():
         final_output = merge_output((heigth, width), out_dir)
         final_output = (final_output > args.threshold).astype(np.uint8)
         final_output = final_output * 255
-        cv2.imwrite(f'{args.output_folder}/{scene.split(".")[0]}_predicted.png', final_output)
+
+        # write alpha layer
+        alpha_layer = np.zeros(raw.shape, dtype=np.uint8)
+        alpha_layer[final_output > 0, :] = (45, 255, 45)
+        raw = cv2.addWeighted(raw, 0.65, alpha_layer, 0.3, 0)
+        cv2.imwrite(f'{args.output_folder}/scene_masks/{model_name[:-4]}_{scene.split(".")[0]}_tta{args.tta}_predicted.png', raw)
 
         # get IoU and DICE
         final_output = (final_output > 0).astype(np.uint8)
@@ -123,8 +134,8 @@ def main():
         intersection = (final_output * mask).sum()
         cardinality = (final_output + mask).sum()
         union = np.logical_or(final_output, mask).sum() 
-        iou = intersection / (union + 1E-6)
-        dice = 2 * intersection / (cardinality + 1E-6)
+        iou = (intersection + 1) / (union + 1)
+        dice = (2 * intersection + 1) / (cardinality + 1)
 
         # erase temp tiles
         if not args.save_output:
@@ -133,9 +144,16 @@ def main():
         model_stats = model_stats.append({'model': model_name, 
                                           'iou': iou, 
                                           'dice': dice,
-                                          'scene': scene}, ignore_index=True)
+                                          'scene': scene,
+                                          'tta': args.tta == 1}, ignore_index=True)
 
-    model_stats.to_csv(f'{args.output_folder}/model_stats.csv', index=False)
+    # log results
+    model_stats.to_csv(f'{args.output_folder}/{model_name[:-4]}/model_stats.csv', index=False)
+    global_stats = global_stats.append({'model_name': model_name,
+                                        'tta': args.tta == 1,
+                                        'mean_iou': model_stats.iou.mean(),
+                                        'mean_f1': model_stats.dice.mean()}, ignore_index=True)
+    global_stats.to_csv(f'{args.output_folder}/global_stats.csv', index=False)
 
 
 if __name__ == '__main__':
