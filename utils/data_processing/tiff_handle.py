6a5d821da62d7d1d065b232a8418b831578f047e
bentocg <bentocollares@gmail.com>
2022-09-28 05:28:15 -0400

    Fixed width and height for panchromatic scenes

--- utils/data_processing/tiff_handle.py
+++ utils/data_processing/tiff_handle.py
@@ -12,6 +12,9 @@ class Tiff:
     def process_raster(raster_path, mask_path) -> np.array:
         if 'WV03' in raster_path:
             with rasterio.open(raster_path) as src:
+                width = src.width
+                height = src.height
+                meta = src.meta
                 if mask_path:
                     # Mask out land
                     with fiona.open(mask_path, "r") as shapefile:
@@ -22,11 +25,12 @@ class Tiff:
                     if "M1BS" in raster_path:
                         img = src.read([2, 3, 5]).transpose(1, 2, 0)
                     else:
-                        img = src.read(1).reshape(src.height, src.width, 1)[::4, ::4, :]
+                        img = src.read(1).reshape(height, width, 1)[::4, ::4, :]
+                        height = height // 4
+                        width = width // 4
+
                 
         else:
             raise NotImplementedError(f'sensor not supported for {raster_path}')
-        width = src.width
-        height = src.height
-        meta = src.meta
+
         return img, width, height, meta
