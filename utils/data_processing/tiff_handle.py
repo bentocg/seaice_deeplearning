06b21778c55ca11f4abb6155a6c36a25b6d4dfdf
bentocg <bentocollares@gmail.com>
2022-10-07 05:44:18 -0400

    Added support for 3-channel tiffs

--- utils/data_processing/tiff_handle.py
+++ utils/data_processing/tiff_handle.py
@@ -10,7 +10,13 @@ class Tiff:
     def process_raster(raster_path) -> np.array:
         if 'WV03' in raster_path:
             with rasterio.open(raster_path) as src:
-                img = src.read([2, 3, 5]).transpose(1, 2, 0)
+                if src.count == 8:
+                    img = src.read([2, 3, 5]).transpose(1, 2, 0)
+                elif src.count == 3:
+                    img = src.read([1, 2, 3]).transpose(1, 2, 0)
+                else:
+                    raise ValueError(f"Number of layers: {src.count} not supported.")
+
                 
         else:
             raise NotImplementedError(f'sensor not supported for {raster_path}')
